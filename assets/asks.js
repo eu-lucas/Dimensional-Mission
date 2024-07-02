88feaf519785518024341bd8a9050e14f77d66b2
lucaas-tavares <eu.ikki.dev@gmail.com>
2024-07-02 20:04:41 -0300

    feat: to add new features to the project - Button to start or run. - Added replay button. - Function to replace names. - Start and play button again with new look.

--- assets/asks.js
+++ assets/asks.js
@@ -1,95 +1,166 @@
 export const perguntas = [
     {
-        enunciado: "Ao explorar uma antiga ruína em uma floresta mística, você encontra um portal brilhante que o transporta para outra dimensão. Ao atravessa-lo, você se depara com uma criatura falante que diz poder realizar todos os seus desejos. Qual é o seu primeiro pensamento?",
+        enunciado: "Ao atravessar o portal que apareceu em sua escola, você se encontra em uma dimensão onde a gravidade é diferente. Você flutua levemente e vê uma criatura mais bizarra que qualquer pessoa da sua turma, ainda mais que people. O que você faz?",
         alternativas: [
             {
-                texto: "Wow, isso é assustador!",
+                texto: "Tentar estabelecer comunicação telepática com a criatura.",
                 afirmacao: [
-                    "Você se sente um pouco assustado com isso.",
-                    "Questiona-se: \"Isso é real? Como isso é possível?\" enquanto encara a criatura."
-                ]
+                    "Ao atravessar o portal, se depara com uma criatura estranha e percebe que a concentração em pensamentos amigáveis parece funcionar com essa criatura.",
+                    "Ao atravessar o portal, se depara com uma criatura estranha, e ao gritar 'quem é você', a criatura responde e um entendimento começa a se formar."
+                ],
+                proxima: 1,
             },
             {
-                texto: "É mesmo possível? quero testar!",
+                texto: "Ficar calado e observar a criatura de longe.",
                 afirmacao: [
-                    "Você fica maravilhado e imediatamente pensa em fazer um desejo.",
-                    "Entusiasmado, você decide testar os poderes da criatura e conhece-la um pouco mais."
-                ]
-            }           
-        ]
-    },
-    {
-        enunciado: "Você continua sua jornada e chega a uma dimensão onde o tempo flui de maneira diferente. Aqui, o dia e a noite duram apenas algumas horas, e o ambiente está em constante mudança. Como você decide explorar essa dimensão?",
-        alternativas: [
-            {
-                texto: "Investigar as mudanças do ambiente e tentar entender o ciclo temporal.",
-                afirmacao: [
-                    "Você observa atentamente as transições rápidas entre dia e noite.",
-                    "Descobre padrões interessantes que podem ser úteis em sua jornada."
-                ]
-            },
-            {
-                texto: "Buscar abrigo e esperar até que o ambiente se estabilize.",
-                afirmacao: [
-                    "Você encontra um local seguro para se proteger das mudanças constantes.",
-                    "Utiliza o tempo no abrigo para planejar seus próximos passos com calma."
-                ]
+                    "Ao atravessar o portal, se depara com uma criatura estranha e decide manter uma distância segura para estudar a criatura.",
+                    "Ao atravessar o portal, se depara com uma criatura estranha, se esconde e observa a criatura a tarde toda, concluindo que ela parece estar guardando algo importante."
+                ],
+                proxima: 1,
             }
         ]
     },
     {
-        enunciado: "Em uma dimensão subaquática, você encontra uma cidade antiga submersa. Para explorar a cidade, você precisa escolher um meio de locomoção. Qual você escolhe?",
+        enunciado: "Você decide ir para outro lugar, mas acaba percebendo que o tempo parece estar congelado somente em locais infestados de pequenos cogumelos. Você também percebe que tem vários cadáveres de seres vivos com esses cogumelos brotando de dentro de seus corpos. Qual é a sua reação?",
         alternativas: [
             {
-                texto: "Usar um traje de mergulho especial que permite respirar debaixo d'água.",
+                texto: "Verificar os cadáveres para ver se encontra uma resposta.",
                 afirmacao: [
-                    "Você veste o traje e se maravilha com a beleza da cidade submersa.",
-                    "Descobre ruínas antigas e segredos escondidos nas profundezas."
-                ]
+                    "Cansado de tanto observar a criatura, decide analisar os cadáveres que estão espalhados pelos cantos. Esses cadáveres aparentam estar infectados.",
+                    "Enquanto observava a criatura, não pôde deixar de perceber que à esquerda havia um mar de cogumelos e vários cadáveres com esses cogumelos brotando deles. Entusiasmado, decide analisar os cadáveres e conclui que parece ser uma infecção."
+                ],
+                proxima: 2,
             },
             {
-                texto: "Montar em uma criatura marinha amigável que conhece bem a cidade.",
+                texto: "Correr para o mais longe possível.",
                 afirmacao: [
-                    "A criatura te guia pelas áreas mais incríveis da cidade submersa.",
-                    "Aprende sobre a história da cidade através dos olhos da criatura."
-                ]
+                    "Enquanto olhava a criatura, percebeu que à esquerda de onde estava havia vários cadáveres em um mar de cogumelos. Assustado com tudo isso, correu para o mais longe que conseguiu.",
+                    "Enquanto olhava a criatura, percebeu que à esquerda havia vários cadáveres e cogumelos assustadores brotando desses cadáveres. A mente não respondia mais por si, então seu corpo automaticamente fugiu, pois percebeu que algo de bom não tinha para aquele lado."
+                ],
+                proxima: 2,
             }
         ]
     },
     {
-        enunciado: "Ao chegar em uma dimensão de deserto interminável, você encontra uma caravana de nômades que se oferece para te guiar. Qual é a sua abordagem?",
+        enunciado: "Ao continuar explorando, você chega a um vale dominado por uma vegetação exótica e luminosa. O ar está carregado de energia e você sente que algo antigo e poderoso está escondido por ali. Como você decide proceder?",
         alternativas: [
             {
-                texto: "Aceitar a ajuda dos nômades e viajar com a caravana.",
+                texto: "Investigar a vegetação em busca de pistas sobre a fonte de energia.",
                 afirmacao: [
-                    "Você se junta à caravana e aprende sobre as tradições e habilidades dos nômades.",
-                    "Descobre o oásis secreto que eles protegem e compartilham apenas com os dignos."
-                ]
+                    "A vegetação parece reagir à sua presença, conduzindo-o a um antigo artefato escondido no coração do vale.",
+                    "Entre as plantas luminescentes, encontra-se uma inscrição que revela segredos sobre a árvore conhecida como coração da dimensão."
+                ],
+                proxima: 3,
             },
             {
-                texto: "Recusar a ajuda e tentar atravessar o deserto sozinho.",
+                texto: "Procurar um caminho que leve diretamente à árvore coração da dimensão.",
                 afirmacao: [
-                    "Você decide seguir seu próprio caminho, confiando em sua intuição.",
-                    "Enfrenta desafios extremos, mas ganha uma experiência inestimável."
-                ]
+                    "Seguir um caminho estreito entre a vegetação leva a um portal que se abre para uma visão direta da árvore coração da dimensão.",
+                    "O caminho escolhido revela ser a rota mais segura para alcançar a árvore coração da dimensão, evitando áreas infestadas de cogumelos."
+                ],
+                proxima: 3,
             }
         ]
     },
     {
-        enunciado: "Finalmente, você chega a uma dimensão onde cada pensamento seu se materializa instantaneamente. Como você lida com esse poder?",
+        enunciado: "A vegetação começa a ficar mais densa e escura, e você encontra um grupo de moradores locais, visivelmente aflitos. Eles estão tentando conter a propagação dos cogumelos. O que você faz?",
         alternativas: [
             {
-                texto: "Focar em pensamentos positivos e criativos para moldar o ambiente ao seu redor.",
+                texto: "Oferecer ajuda aos moradores para conter a infestação.",
                 afirmacao: [
-                    "Você cria um ambiente harmonioso e cheio de maravilhas.",
-                    "Descobre que seus pensamentos podem influenciar não só o ambiente, mas também as criaturas ao seu redor."
+                    "Trabalhar junto com os moradores fortalece seu vínculo com eles, e você aprende mais sobre os métodos de contenção dos cogumelos.",
+                    "A ajuda prestada revela informações valiosas sobre o próximo passo necessário para salvar a árvore coração da dimensão."
+                ],
+                proxima: 4,
+            },
+            {
+                texto: "Perguntar aos moradores sobre um caminho alternativo até a árvore coração da dimensão.",
+                afirmacao: [
+                    "Os moradores indicam um caminho secreto que contorna as áreas mais perigosas e infestadas.",
+                    "As informações dos moradores sobre o caminho alternativo revelam ser cruciais para evitar armadilhas e chegar mais rápido à árvore coração da dimensão."
+                ],
+                proxima: 4,
+            }
+        ]
+    },
+    {
+        enunciado: "Seguindo o caminho indicado pelos moradores, você encontra uma caverna oculta que emite uma luz intensa. Ao entrar, você percebe que essa caverna é um ponto de convergência de energia. O que você faz?",
+        alternativas: [
+            {
+                texto: "Explorar a caverna em busca de itens ou pistas que possam ajudar na sua jornada.",
+                afirmacao: [
+                    "Dentro da caverna, vários cristais energizados são encontrados, cada um com propriedades únicas que podem ser usadas contra a infestação.",
+                    "Uma inscrição na parede da caverna revela mais segredos sobre como chegar à árvore coração da dimensão."
+                ],
+                proxima: 5,
+            },
+            {
+                texto: "Usar a energia da caverna para se fortalecer antes de continuar a jornada.",
+                afirmacao: [
+                    "A energia da caverna infunde seu corpo, aumentando suas habilidades e resistência contra os cogumelos.",
+                    "Com o poder aumentado, você sente que está mais preparado para enfrentar os desafios à frente e salvar a dimensão."
+                ],
+                proxima: 5,
+            }
+        ]
+    },
+    {
+        enunciado: "Ao sair da caverna, você se depara com um campo de batalha antigo, onde vestígios de uma luta passada contra a infestação de cogumelos estão espalhados. O que você faz?",
+        alternativas: [
+            {
+                texto: "Examinar os vestígios para aprender com as batalhas passadas.",
+                afirmacao: [
+                    "Os vestígios revelam táticas antigas e eficazes contra a infestação, que podem ser utilizadas na batalha final.",
+                    "Entre os restos, um artefato poderoso é encontrado, projetado especificamente para combater os cogumelos."
+                ],
+                proxima: 6,
+            },
+            {
+                texto: "Buscar uma rota segura através do campo de batalha para evitar novos confrontos.",
+                afirmacao: [
+                    "Uma rota segura é traçada, evitando áreas perigosas e infestadas.",
+                    "Essa rota leva diretamente à área onde a árvore coração da dimensão pode ser avistada ao longe."
+                ],
+                proxima: 6,
+            }
+        ]
+    },
+    {
+        enunciado: "Finalmente, você chega à área onde a árvore coração da dimensão está localizada. No entanto, o local está cercado por cogumelos gigantes e agressivos que parecem estar sugando sua energia. Qual é a sua estratégia?",
+        alternativas: [
+            {
+                texto: "Usar as habilidades e itens adquiridos para combater os cogumelos e proteger a árvore.",
+                afirmacao: [
+                    "Os cristais energizados e o artefato antigo são usados com sucesso para combater os cogumelos.",
+                    "Os moradores, vendo sua bravura, se juntam na batalha final, expulsando os cogumelos e salvando a árvore."
+                ],
+                proxima: 7,
+            },
+            {
+                texto: "Buscar uma forma de redirecionar a energia da árvore para repelir os cogumelos.",
+                afirmacao: [
+                    "A energia da árvore é redirecionada de maneira eficaz, criando uma barreira que impede os cogumelos de se aproximarem.",
+                    "Com a barreira energética, a árvore começa a se regenerar e a infestação é contida."
+                ],
+                proxima: 7,
+            }
+        ]
+    },
+    {
+        enunciado: "Com a infestação contida e a árvore coração da dimensão protegida, você percebe que a energia ao redor começou a estabilizar. Os portais de saída começam a se formar. O que você faz?",
+        alternativas: [
+            {
+                texto: "Agradecer aos moradores e se preparar para retornar à sua dimensão.",
+                afirmacao: [
+                    "Os moradores expressam sua gratidão e fornecem itens que podem ser úteis no futuro.",
+                    "Com uma sensação de dever cumprido, você atravessa o portal de volta à sua escola."
                 ]
             },
             {
-                texto: "Tentar controlar seus pensamentos para evitar que algo perigoso se materialize.",
+                texto: "Pedir aos moradores para aprender mais sobre a dimensão antes de partir.",
                 afirmacao: [
-                    "Você se esforça para manter a mente calma e controlada.",
-                    "Aprende técnicas de meditação para ajudar a manter o foco e a segurança."
+                    "Os moradores compartilham histórias antigas e conhecimentos profundos sobre a dimensão.",
+                    "Com novas habilidades e conhecimentos, você atravessa o portal, sabendo que pode voltar no futuro."
                 ]
             }
         ]
--- assets/main.js
+++ assets/main.js
@@ -1,5 +1,5 @@
 import { perguntas } from "./asks.js";
-import { aleatorio } from "./random.js";
+import { aleatorio, nome, pessoaAleatoria, posicaoPessoaAleatoria } from "./random.js";
 
 const caixaPrincipal = document.querySelector(".caixa-principal");
 const caixaPerguntas = document.querySelector(".caixa-perguntas");
@@ -7,10 +7,24 @@ const caixaAlternativas = document.querySelector(".caixa-alternativas");
 const caixaResultado = document.querySelector(".caixa-resultado");
 const textoResultado = document.querySelector(".texto-resultado");
 const botaoJogarNovamente = document.querySelector(".novamente-btn");
+const botaoIniciar = document.querySelector(".inicia-btn")
+const telaInicial = document.querySelector(".tela-inicial")
 
 let atual = 0; 
 let perguntaAtual;
 let historiaFinal = "";
+
+botaoIniciar.addEventListener("click", iniciaJogo);
+
+function iniciaJogo(){
+    let atual = 0; 
+    let historiaFinal = "";
+    telaInicial.style.display = 'none';
+    caixaPerguntas.classList.remove("mostrar");
+    caixaAlternativas.classList.remove("mostrar");
+    caixaResultado.classList.remove("mostrar");
+    mostraPergunta()
+}
 
 function mostraPergunta() {
     if(atual >= perguntas.length){
@@ -35,21 +49,41 @@ function mostraAlternativas(){
 function respostaSelecionada(opcaoSelecionada){
     const afirmacoes = aleatorio(opcaoSelecionada.afirmacao);
     historiaFinal += afirmacoes + " ";
-    atual++;
+    if(opcaoSelecionada.proxima !== undefined){
+        atual = opcaoSelecionada.proxima;
+    } else {
+        mostraResultado();
+        return;
+    }
     mostraPergunta();
 }
 
 function mostraResultado(){
-    caixaPerguntas.textContent = "Ao se deparar com a criatura...";
+    caixaPerguntas.textContent = `${nome}, atravessou o portal...`;
     textoResultado.textContent = historiaFinal;
     caixaAlternativas.textContent = ""; 
-    botaoJogarNovamente.addEventListener("click", jogarNovamente());
+    caixaResultado.classList.add("mostrar");
+    botaoJogarNovamente.addEventListener("click", jogarNovamente);
 }
 
 function jogarNovamente() {
     atual = 0;
     historiaFinal = "";
-    mostraPergunta()
+    caixaResultado.classList.remove("mostrar");
+    mostraPergunta();
 }
 
-mostraPergunta();
+function obterPronome(posicao) {
+    const posicaoPessoa = [0, 5, 7, 8, 10, 13];
+    return posicaoPessoa.includes(posicao) ? "a" : "o";
+}
+
+function substituirNome(){
+    const pronome = obterPronome(posicaoPessoaAleatoria);
+    for(const pergunta of perguntas){
+        pergunta.enunciado = pergunta.enunciado.replace(/você/g, nome);
+        pergunta.enunciado = pergunta.enunciado.replace(/people/g, `${pronome} ${pessoaAleatoria}`); // gambiarra braba, mas funcional
+    }
+}
+
+substituirNome();
